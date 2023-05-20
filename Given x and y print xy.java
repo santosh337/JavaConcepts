dd17acf0a5e57afc2c75609dc2bb2b6f92f6e817
Santosh Sugur <santosh.iosdev@gmail.com>
2023-05-20 18:40:30 +0000

    Create Given x and y print xy.java

---
+++ Given x and y print xy.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        int n = scn.nextInt();
+        
+        for (int i=1;i<=n;i++) {
+            int x = scn.nextInt();
+            int y = scn.nextInt();
+            
+            System.out.println(x+""+y);
+            
+        }
+    }
+}
